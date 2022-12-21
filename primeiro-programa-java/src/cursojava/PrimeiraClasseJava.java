2915e3a593eee859dfa58a05571c010594d5441f
nixcodedev <nixon.codedev@gmail.com>
2022-12-21 18:00:56 -0300

    Parcial da Aula - Operações Lógicas com IF e ELSE

--- primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
@@ -1,24 +1,21 @@
 package cursojava;
 
 public class PrimeiraClasseJava {
-	
-	/*Main é m método auto executável em Java*/
+
+	/* Main é m método auto executável em Java */
 	public static void main(String[] args) {
-	
-		/*Concatenação = Unir ou juntar dados*/
-		String nome = ("Nixon Sena");
-		String cpf = ("488.789.789-85");
-		String telefone = ("75 98700-1076");
-		String endereco = ("Valença-BA");
-		int idade = 41;
-		
-		String saida = "Meu nome é: " + nome + ", meu CPF é: " +
-		cpf + ", Meu contato é: " + telefone +
-		", eu moro em: " + endereco + " e a minha idade é: " + idade + ".";
-		
-		System.out.println(saida);
-		
-		//TESTE MAYCON
+
+		int mediaAluno = 70;
+		String nome = "Alex";
+
+		if (mediaAluno >= 70 && nome.equals("Nixon")) {
+			System.out.println("Parabéns, você está aprovado!");
+		} else if (mediaAluno < 70) {
+			System.out.println("Aluno reprovado.");
+
+		} else {
+			System.out.println("Aluno não encontrado.");
+		}
+
 	}
-	
 }
