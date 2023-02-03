f390f5d11c5c27fe538675fc3495b8092f041f43
nixcodedev <nixon.codedev@gmail.com>
2023-02-03 17:48:31 -0300

    AULA - Operações lógicas com IF e ELSE

--- primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
@@ -5,17 +5,23 @@ public class PrimeiraClasseJava {
 	/* Main é m método auto executável em Java */
 	public static void main(String[] args) {
 
-		int mediaAluno = 70;
-		String nome = "Alex";
-
-		if (mediaAluno >= 70 && nome.equals("Nixon")) {
-			System.out.println("Parabéns, você está aprovado!");
-		} else if (mediaAluno < 70) {
-			System.out.println("Aluno reprovado.");
-
-		} else {
-			System.out.println("Aluno não encontrado.");
+		int nota1 = 10;
+		int nota2 = 60;
+		int nota3 = 10;
+		int nota4 = 10;
+		int media = 0;
+		
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		if (media >= 70) {
+			System.out.println("Aluno aprovado: " + media);
+		}else if (media >= 40 && media <= 69) {
+			 System.out.println("Aluno em recuperação: " + media);
+		 }  
+		else {
+			System.out.println("Aluno reprovado: " + media);
 		}
-
+		
+	
 	}
 }
