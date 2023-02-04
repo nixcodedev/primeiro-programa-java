2258d076fc613492be390abead02579cbd3a8e5d
nixcodedev <nixon.codedev@gmail.com>
2023-02-04 11:59:00 -0300

    Operações lógicas aninhadas

--- primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
@@ -5,30 +5,29 @@ public class PrimeiraClasseJava {
 	/* Main é o método auto executável em Java */
 	public static void main(String[] args) {
 
-		int nota1 = 70;
-		int nota2 = 60;
-		int nota3 = 90;
+		int nota1 = 90;
+		int nota2 = 90;
+		int nota3 = 95;
 		int nota4 = 90;
 		int media = 0;
 		
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
 		
-		/*Condições lógicas com IF e ELSE*/
-		/*if (media >= 70) {
-			System.out.println("Aluno aprovado: " + media);
-		}else if (media >= 40 && media <= 69) {
-			 System.out.println("Aluno em recuperação: " + media);
-		 }  
-		else {
-			System.out.println("Aluno reprovado: " + media);
-		}*/
+		/*Operações lógicas aninhadas: São operações dentro de operações*/
 		
-		/*Operadores ternários são para micro validações*/
+		if (media >= 50 ) {
+			if (media >= 70) {
+				if (media > 90) {
+					System.out.println("Aluno está aprovado direto - Parabéns! " + media);
+				}else {
+					System.out.println("Aluno está aprovado direto " + media);
+			    }
+			}else {
+				System.out.println("Aluno está em recuperação " + media);
+		    }
+		}else {
+			System.out.println("Aluno reprovado direto " + media);
+		}
 		
-		String saidaResultado = media >= 70 ? "Aluno Aprovado" : (media >= 40 && media <= 69) ? "Aluno em recuperação" : "Reprovado";
-		
-		System.out.println(saidaResultado);
-		
-	
-	}
+}
 }
