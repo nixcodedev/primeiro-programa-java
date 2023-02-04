5a809652807a828dbf7ab337ed9760297c664017
nixcodedev <nixon.codedev@gmail.com>
2023-02-04 12:23:46 -0300

    AULA - O comando Switch e Case

--- primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
@@ -4,29 +4,30 @@ public class PrimeiraClasseJava {
 
 	/* Main é o método auto executável em Java */
 	public static void main(String[] args) {
-
-		int nota1 = 90;
-		int nota2 = 90;
-		int nota3 = 95;
-		int nota4 = 90;
+/*
+		int nota1 = 70;
+		int nota2 = 70;
+		int nota3 = 70;
+		int nota4 = 70;
 		int media = 0;
 		
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
+*/
 		
-		/*Operações lógicas aninhadas: São operações dentro de operações*/
-		
-		if (media >= 50 ) {
-			if (media >= 70) {
-				if (media > 90) {
-					System.out.println("Aluno está aprovado direto - Parabéns! " + media);
-				}else {
-					System.out.println("Aluno está aprovado direto " + media);
-			    }
-			}else {
-				System.out.println("Aluno está em recuperação " + media);
-		    }
-		}else {
-			System.out.println("Aluno reprovado direto " + media);
+		/*SWITCH CASE: OPERAÇÕES EXATAS*/
+		int dia = 3;
+		switch (dia) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda-feira");
+			break;
+		case 3:
+			System.out.println("Terça-feira");
+			break;
+		default: System.out.println("Outro dia qualquer");
+			break;
 		}
 		
 }
