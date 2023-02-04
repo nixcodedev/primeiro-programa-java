e6513ea97940bc05a43f715856efd13b3bf0bc06
nixcodedev <nixon.codedev@gmail.com>
2023-02-04 12:50:01 -0300

    AULA - Operadores relacionais

--- primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
@@ -4,30 +4,14 @@ public class PrimeiraClasseJava {
 
 	/* Main é o método auto executável em Java */
 	public static void main(String[] args) {
-/*
-		int nota1 = 70;
-		int nota2 = 70;
-		int nota3 = 70;
-		int nota4 = 70;
-		int media = 0;
+
+		int nota1 = 50;
+		int nota2 = 91;
 		
-		media = (nota1 + nota2 + nota3 + nota4) / 4;
-*/
-		
-		/*SWITCH CASE: OPERAÇÕES EXATAS*/
-		int dia = 3;
-		switch (dia) {
-		case 1:
-			System.out.println("Domingo");
-			break;
-		case 2:
-			System.out.println("Segunda-feira");
-			break;
-		case 3:
-			System.out.println("Terça-feira");
-			break;
-		default: System.out.println("Outro dia qualquer");
-			break;
+		if (nota1 == nota2) {
+			System.out.println("A nota 1 é menor e igual");
+		}else {
+			System.out.println("A nota 1 é maior");
 		}
 		
 }
