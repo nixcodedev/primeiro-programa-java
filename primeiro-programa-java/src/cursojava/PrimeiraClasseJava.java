2c4811ecdf3f016f0a64cec652a08e0942147ce5
nixcodedev <nixon.codedev@gmail.com>
2023-02-06 16:03:38 -0300

    AULA - Operadores relacionais

--- primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
@@ -5,13 +5,13 @@ public class PrimeiraClasseJava {
 	/* Main é o método auto executável em Java */
 	public static void main(String[] args) {
 
-		int nota1 = 50;
-		int nota2 = 91;
+		int nota1 = 100;
+		int nota2 = 100;
 		
-		if (nota1 == nota2) {
-			System.out.println("A nota 1 é menor e igual");
+		if (nota1 > nota2) {
+			System.out.println("A nota 1 é maior e igual");
 		}else {
-			System.out.println("A nota 1 é maior");
+			System.out.println("As notas são iguais");
 		}
 		
 }
