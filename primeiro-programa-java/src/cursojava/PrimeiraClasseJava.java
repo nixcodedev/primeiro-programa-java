aed6b336a406a0240798b6a81b5da010cee75a3d
nixcodedev <nixon.codedev@gmail.com>
2023-02-06 17:01:30 -0300

    AULA - Estrutura de repetição WHILE e DO WHILE

--- primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
@@ -5,14 +5,24 @@ public class PrimeiraClasseJava {
 	/* Main é o método auto executável em Java */
 	public static void main(String[] args) {
 
-		int nota1 = 100;
-		int nota2 = 100;
-		
-		if (nota1 > nota2) {
-			System.out.println("A nota 1 é maior e igual");
-		}else {
-			System.out.println("As notas são iguais");
+		/* Estrutura de repetição While */
+
+		int numero = 0;
+
+		while (numero <= 60) { /* Verifica depois que executa */
+			System.out.println("O número atual é: " + numero);
+			numero++;
 		}
-		
-}
+
+		/*--------------------------------------------------------*/
+
+		int numero2 = 0;
+		do { /* primeiro executa e depois verifica */
+
+			System.out.println("O número atual é: " + numero2);
+			numero2++;
+
+		} while (numero2 <= 60);
+
+	}
 }
