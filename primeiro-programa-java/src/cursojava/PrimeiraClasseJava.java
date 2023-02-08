2aaf552bea6f262aa82734b1891ed68f9a9f8b88
nixcodedev <nixon.codedev@gmail.com>
2023-02-08 12:30:54 -0300

    AULA - Estrutura de repetição e o Continue

--- primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
@@ -5,13 +5,12 @@ public class PrimeiraClasseJava {
 	/* Main é o método auto executável em Java */
 	public static void main(String[] args) {
 
-		/* Estrutura de repetição FOR com Break (Parada) */
+		/* Estrutura de repetição FOR e Continue */
 
-		for (int numero = 0; numero <= 10; numero ++) {
-			if (numero == 3) {
-				System.out.println("Blz, encontrei o número 3.");
-				System.out.println("Estou parando de executar...");
-				break;
+		for (int numero = 0; numero <= 10; numero++) {
+			if (numero == 3 || numero == 6 || numero == 9) {
+				System.out.println("Blz, encontrei o número: " + numero);
+				continue;
 			}
 		}
 
