38fcf77c58322d04b2b6996a6d5cd77d4f1ecccc
nixcodedev <nixon.codedev@gmail.com>
2023-02-09 11:59:36 -0300

    AULA - Criando uma opção de confirmação

--- primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/PrimeiraClasseJava.java
@@ -1,19 +1,24 @@
 package cursojava;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 
 	/* Main é o método auto executável em Java */
 	public static void main(String[] args) {
 
-		/* Módulo: Restp da divisão % */
-		
-		double carro = 9;
-		double pessoa = 2;
-		
-		double resto = carro % pessoa; /* "%" OPERADOR DE MÓDULO OU MOD*/
-		
-		System.out.println("Sobraram exatamente: " + resto + " carros.");
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros: ");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas: ");
 
+		/*Conversão de dado String em numero*/
+		double carroNumero = Double.parseDouble(carros);
+		double pessoaNumero = Double.parseDouble(pessoas);
+
+		int divisao = (int) (carroNumero / pessoaNumero);
+
+		double resto = carroNumero % pessoaNumero;
+
+		JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + " carros e sobrou " + resto + " carro(s).");
 
 	}
 }
