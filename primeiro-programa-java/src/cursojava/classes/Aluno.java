914142cece241ffede0ab045b6b99efb5b4930d3
nixcodedev <nixon.codedev@gmail.com>
2023-02-13 17:31:19 -0300

    AULA - Primeira Classe e Objeto Aluno

---
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,19 @@
+package cursojava.classes;
+
+/*Esta é a nossa classe/objeto que representa o Aluno*/
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+	
+
+}
