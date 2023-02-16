5d379956d109007a9eac04eaf2e731ed75bbadbb
nixcodedev <nixon.codedev@gmail.com>
2023-02-16 15:56:45 -0300

    AULA - Instanciação, atributos e referências

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -9,14 +9,16 @@ public class PrimeiraClasseJava {
 	/* Main é o método auto executável em Java */
 	public static void main(String[] args) {
 
-		Aluno aluno1 = new Aluno(); //Aqui será o aluno João
-				
-		Aluno aluno2 = new Aluno(); //Aqui será o aluno Pedro
-		
-		Aluno aluno3 = new Aluno(); //Aqui será o aluno Alex
+		/* new Aluno() é uma instância (Criação de Objeto) */
+		/*aluno1 é uma referência para o objeto Aluno (variável que representa o Aluno)*/
+		Aluno aluno1 = new Aluno(); // Aqui será o aluno João
+
+		Aluno aluno2 = new Aluno(); // Aqui será o aluno Pedro
+
+		Aluno aluno3 = new Aluno(); // Aqui será o aluno Alex
 
 		Aluno aluno4 = new Aluno("Maria");
-		
+
 		Aluno aluno5 = new Aluno("José", 50);
 
 	}
