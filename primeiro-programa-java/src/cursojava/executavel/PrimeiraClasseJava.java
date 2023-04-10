0ea0097bf2756296bcee11f6f87995e5ce0be857
nixcodedev <nixon.codedev@gmail.com>
2023-04-10 17:51:15 -0300

    AULA - Criando a lista dinâmica das disciplinas do aluno

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -26,8 +26,6 @@ public class PrimeiraClasseJava {
 		String serie = JOptionPane.showInputDialog("Qual a série?");
 		String escola = JOptionPane.showInputDialog("Qual o nome da escola?");
 
-		
-
 		Aluno aluno1 = new Aluno();
 
 		aluno1.setNome(nome);
@@ -41,26 +39,17 @@ public class PrimeiraClasseJava {
 		aluno1.setSerieMatriculado(serie);
 		aluno1.setNomeEscola(escola);
 
-		Disciplina disciplina1 = new Disciplina();
-		disciplina1.setDisciplina("Banco de Dados");
-		disciplina1.setNota(90);
+		for (int pos = 1; pos <= 4; pos++) {
+			String nomeDisciplina = JOptionPane.showInputDialog("Nome da Disciplina " + pos + "?");
+			String notaDisciplina = JOptionPane.showInputDialog("Nota da Disciplina " + pos + "?");
 
-		Disciplina disciplina2 = new Disciplina();
-		disciplina2.setDisciplina("Matemática");
-		disciplina2.setNota(80);
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(nomeDisciplina);
+			disciplina.setNota(Double.valueOf(notaDisciplina));
 
-		Disciplina disciplina3 = new Disciplina();
-		disciplina3.setDisciplina("Geografia");
-		disciplina3.setNota(97);
+			aluno1.getDisciplinas().add(disciplina);
 
-		Disciplina disciplina4 = new Disciplina();
-		disciplina4.setDisciplina("Java Web");
-		disciplina4.setNota(70);
-
-		aluno1.getDisciplinas().add(disciplina1);
-		aluno1.getDisciplinas().add(disciplina2);
-		aluno1.getDisciplinas().add(disciplina3);
-		aluno1.getDisciplinas().add(disciplina4);
+		}
 
 		System.out.println(aluno1.toString()); // Descrição do objeto na memória
 		System.out.println("Média do aluno: " + aluno1.getMediaNota());
