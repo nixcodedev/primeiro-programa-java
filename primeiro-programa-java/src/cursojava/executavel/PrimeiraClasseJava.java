961759fd2c6a70dc3f535da25012e125f4b1bef8
nixcodedev <nixon.codedev@gmail.com>
2023-04-13 12:19:14 -0300

    AULA - Remover disciplinas em repetição

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -16,7 +16,7 @@ public class PrimeiraClasseJava {
 		 */
 
 		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
-		String idade = JOptionPane.showInputDialog("Qual a idade do aluno?");
+		/*String idade = JOptionPane.showInputDialog("Qual a idade do aluno?");
 		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento?");
 		String rg = JOptionPane.showInputDialog("Digite o número do RG.");
 		String cpf = JOptionPane.showInputDialog("Digite o CPF");
@@ -24,12 +24,12 @@ public class PrimeiraClasseJava {
 		String pai = JOptionPane.showInputDialog("Qual nome do pai?");
 		String matricula = JOptionPane.showInputDialog("Qual a data da matrícula?");
 		String serie = JOptionPane.showInputDialog("Qual a série?");
-		String escola = JOptionPane.showInputDialog("Qual o nome da escola?");
+		String escola = JOptionPane.showInputDialog("Qual o nome da escola?");*/
 
 		Aluno aluno1 = new Aluno();
 
 		aluno1.setNome(nome);
-		aluno1.setIdade(Integer.valueOf(idade)); // Convertendo string em inteiro
+		/*aluno1.setIdade(Integer.valueOf(idade)); // Convertendo string em inteiro
 		aluno1.setDataNascimento(dataNascimento);
 		aluno1.setRegistroGeral(rg);
 		aluno1.setNumeroCpf(cpf);
@@ -37,7 +37,7 @@ public class PrimeiraClasseJava {
 		aluno1.setNomePai(pai);
 		aluno1.setDataMatricula(matricula);
 		aluno1.setSerieMatriculado(serie);
-		aluno1.setNomeEscola(escola);
+		aluno1.setNomeEscola(escola);*/
 
 		for (int pos = 1; pos <= 4; pos++) {
 			String nomeDisciplina = JOptionPane.showInputDialog("Nome da Disciplina " + pos + "?");
@@ -53,10 +53,18 @@ public class PrimeiraClasseJava {
 
 		/* CONDICIONAL PARA REMOVER DISCIPLINA */
 		int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
-		if (escolha == 0) {
-			String disciplinaRemover = JOptionPane.showInputDialog(null, "Qual a disciplina, 1, 2, 3 ou 4?");
-			aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - 1);
 
+		if (escolha == 0) {/* Opção SIM é Zero */
+
+			int continuarRemover = 0;
+			int posicao = 1;
+
+			while (continuarRemover == 0) {
+				String disciplinaRemover = JOptionPane.showInputDialog(null, "Qual a disciplina, 1, 2, 3 ou 4?");
+				aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
+				posicao ++;/* ++ soma + 1*/
+				continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover?");
+			}
 		}
 
 		System.out.println(aluno1.toString()); // Descrição do objeto na memória
