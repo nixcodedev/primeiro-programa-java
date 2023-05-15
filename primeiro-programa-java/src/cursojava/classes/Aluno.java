1e83f58ca77e81cd25415e311d22f56cc457cb17
nixcodedev <nixon.codedev@gmail.com>
2023-05-15 15:35:59 -0300

    Sintaxe correta da herança

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -6,16 +6,9 @@ import java.util.List;
 import cursojava.constantes.StatusAluno;
 
 /*Esta é a nossa classe/objeto que representa o Aluno*/
-public class Aluno {
+public class Aluno extends Pessoa {
 
 	/* Estes são os atributos do Aluno */
-	private String nome;
-	private int idade;
-	private String dataNascimento;
-	private String registroGeral;
-	private String numeroCpf;
-	private String nomeMae;
-	private String nomePai;
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
