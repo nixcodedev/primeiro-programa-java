e744bd4301b7b32a3c2ca2a2bc86d084608f97f0
nixcodedev <nixon.codedev@gmail.com>
2023-05-30 11:25:31 -0300

    Polimorfismo

--- primeiro-programa-java/src/cursojava/executavel/TestandoClassesFilhas.java
+++ primeiro-programa-java/src/cursojava/executavel/TestandoClassesFilhas.java
@@ -2,6 +2,7 @@ package cursojava.executavel;
 
 import cursojava.classes.Aluno;
 import cursojava.classes.Diretor;
+import cursojava.classes.Pessoa;
 import cursojava.classes.Secretario;
 
 public class TestandoClassesFilhas {
@@ -22,6 +23,7 @@ public class TestandoClassesFilhas {
 		secretario.setExperiencia("Adrministração");
 		secretario.setNumeroCpf("885.835.004-89");
 		secretario.setIdade(18);
+		secretario.setNome("Henrique");
 
 		System.out.println(aluno);
 		System.out.println(diretor);
@@ -34,7 +36,17 @@ public class TestandoClassesFilhas {
 		System.out.println("O salário do Aluno é = R$ " + aluno.salario());
 		System.out.println("O salário do Diretor é = R$ " + diretor.salario());
 		System.out.println("O salário do Secretário é = R$ " + secretario.salario());
-
+		
+		teste(aluno);
+		teste(diretor);
+		teste(secretario);
+		
+	}
+	
+	public static void teste(Pessoa pessoa) {
+		System.out.println("Essa pessoa é demais = " 
+		+ pessoa.getNome() + " e o salário é de = " + pessoa.salario());
+		
 	}
 
 }
