17211d3c5a060eadb0cbd18f18d311c0ab41b110
Nixcodedev <nixon.codedev@gmail.com>
2023-08-10 22:01:33 -0300

    Descobrindo a maior nota do aluno

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -25,21 +25,34 @@ public class ArrayVetor {
 		aluno.getDisciplinas().add(disciplina);
 
 		Disciplina disciplina2 = new Disciplina();
-		disciplina2.setDisciplina("Lógica de Porgamação");
+		disciplina2.setDisciplina("Lógica de Progamação");
 		disciplina2.setNota(notasLogica);
 
 		aluno.getDisciplinas().add(disciplina2);
 
 		System.out.println("Nome do aluno: " + aluno.getNome() + ", inscrito no curso: " + aluno.getNomeEscola() + ".");
 		System.out.println("-------------- Disciplinas do Aluno --------------");
+		
+		
+		/* Percorrendo disciplina e checando a nota máxima */
 		for (Disciplina d : aluno.getDisciplinas()) {
 
 			System.out.println("Disciplina: " + d.getDisciplina());
 			System.out.println("As notas da disciplilna são: ");
 
+			double notaMax = 0.0;
 			for (int pos = 0; pos < d.getNota().length; pos++) {
 				System.out.println("Nota " + (pos + 1) + " é igual a " + d.getNota()[pos]);
+
+				if (pos == 0.0) {
+					notaMax = d.getNota()[pos];
+				} else {
+					if (d.getNota()[pos] > notaMax) {
+						notaMax = d.getNota()[pos];
+					}
+				}
 			}
+			System.out.println("A maior nota da disciplina é: " + d.getDisciplina() + " e de valor: " + notaMax);
 
 		}
 
