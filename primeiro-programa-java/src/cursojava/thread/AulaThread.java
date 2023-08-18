f7fa8bc42c535f38a363a583cef1e02a28d48f0e
nixcodedev <nixon.codedev@gmail.com>
2023-08-18 16:15:30 -0300

    Criando a primeira Thread

--- primeiro-programa-java/src/cursojava/thread/AulaThread.java
+++ primeiro-programa-java/src/cursojava/thread/AulaThread.java
@@ -1,21 +1,43 @@
 package cursojava.thread;
 
+import javax.swing.JOptionPane;
+
 public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
-
-		for (int pos = 0; pos < 10; pos++) {
-			
-			
-			/* Quero executar esse envio com a um tempo de parada ou com um tempo determinado */
-			System.out.println("Executando alguma rotina, por exemplo: envio de e-mail.");
-
-			Thread.sleep(1000); /* Dá um tempo */
 		
-		}
-		
+		/* Thread processando em paralelo */
+		new Thread() {
+			
+			public void run() { /* Executa o que nós queremos  */
+				/* CÓDIGO DA ROTINA */
+				
+				/* Código da rotina que eu quero executar em paralelo */
+				for (int pos = 0; pos < 10; pos++) {
+					
+					/* Quero executar esse envio com a um tempo de parada ou com um tempo determinado */
+					System.out.println("Executando alguma rotina, por exemplo: envio de e-mail.");
+					
+					try {
+						Thread.sleep(1000); /* Dá um tempo */
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} 
+					
+				}
+				/* Fim do código em paralelo */
+				
+			};
+			
+		}.start(); /*Start que liga a thread que fica processando por trás.*/
+
+
+		/* Código do sistema do usuário continua o fluxo do trabalho */
 		System.out.println("\nCHEGOU AO FIM DO CÓDIGO DE TESTE DE TRHEAD.");
-		
+		/*Fluxo do sistema: cadastro de venda, uma emissão de nota fiscal ou algo do tipo...*/
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
+
+
 	}
-	
+
 }
