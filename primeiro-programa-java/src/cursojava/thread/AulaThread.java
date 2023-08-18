852fcf607d3cf981183a88ea47a1ffe7cf273611
nixcodedev <nixon.codedev@gmail.com>
2023-08-18 16:27:22 -0300

    Processamento concorrente entre duas Thread

--- primeiro-programa-java/src/cursojava/thread/AulaThread.java
+++ primeiro-programa-java/src/cursojava/thread/AulaThread.java
@@ -6,7 +6,7 @@ public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		/* Thread processando em paralelo */
+		/* Simulação de Thread processando em paralelo do envio de e-mail*/
 		new Thread() {
 			
 			public void run() { /* Executa o que nós queremos  */
@@ -20,6 +20,35 @@ public class AulaThread {
 					
 					try {
 						Thread.sleep(1000); /* Dá um tempo */
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} 
+					
+				}
+				/* Fim do código em paralelo */
+				
+			};
+			
+		}.start(); /*Start que liga a thread que fica processando por trás.*/
+		
+		
+		//************************************************************************* DIVISÃO DE THREADS
+		
+		
+		/* Simulação de Thread processando em paralelo do envio de nota fiscal*/
+		new Thread() {
+			
+			public void run() { /* Executa o que nós queremos  */
+				/* CÓDIGO DA ROTINA */
+				
+				/* Código da rotina que eu quero executar em paralelo */
+				for (int pos = 0; pos < 10; pos++) {
+					
+					/* Quero executar esse envio com a um tempo de parada ou com um tempo determinado */
+					System.out.println("Executando alguma rotina, por exemplo: envio de nota fiscal.");
+					
+					try {
+						Thread.sleep(4000); /* Dá um tempo */
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					} 
