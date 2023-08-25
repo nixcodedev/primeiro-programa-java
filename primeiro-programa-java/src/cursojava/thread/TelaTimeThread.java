4cd54bd1bdd943435890239c982295d93b6b91a2
nixcodedev <nixon.codedev@gmail.com>
2023-08-25 17:44:45 -0300

    Controlando a Thread 2 com Start e Stop

--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -49,8 +49,30 @@ public class TelaTimeThread extends JDialog {
 		}
 	};
 	
-	private Thread thread1Time;
+	
+	private Runnable thread2 = new Runnable() {
+		
+		@Override
+		public void run() { //Fica sempre rodando
+			while(true) {
+				mostraTempo2.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm:ss").
+						format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+		}
+	};
 
+	
+	private Thread thread1Time;
+	private Thread thread2Time;
+
+	
 	public TelaTimeThread() { /* Executa o que tiver dentro no momento da abertura ou execução */
 
 		setTitle("Minha tela de time com thread"); // titulo da janela
@@ -103,6 +125,12 @@ public class TelaTimeThread extends JDialog {
 				thread1Time = new Thread(thread1);
 				thread1Time.start();
 				
+				thread2Time = new Thread(thread2);
+				thread2Time.start();
+				
+				jButton.setEnabled(false);
+				jButton2.setEnabled(true);
+				
 			}
 		});
 		
@@ -112,9 +140,17 @@ public class TelaTimeThread extends JDialog {
 			public void actionPerformed(ActionEvent e) {
 				
 				thread1Time.stop();
+				thread2Time.stop();
+				
+				jButton.setEnabled(true);
+				jButton2.setEnabled(false);
 				
 			}
 		});
+		
+		
+		
+		jButton2.setEnabled(false);
 
 		add(jPanel, BorderLayout.WEST);
 
