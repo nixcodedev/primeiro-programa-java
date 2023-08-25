f22e9d588fa1c21e42ca38481bfc0b603e1c4690
nixcodedev <nixon.codedev@gmail.com>
2023-08-25 11:55:47 -0300

    Controlando a Thead 1 com Start e Stop

--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -6,6 +6,10 @@ import java.awt.GradientPaint;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -15,73 +19,108 @@ import javax.swing.JTextField;
 import javax.swing.plaf.InsetsUIResource;
 
 public class TelaTimeThread extends JDialog {
-	
-	
-	private JPanel jPanel = new JPanel(new GridBagLayout()); //Painel de componentes
-	
+
+	private JPanel jPanel = new JPanel(new GridBagLayout()); // Painel de componentes
+
 	private JLabel descricaoHora = new JLabel("Time Thread 1");
 	private JTextField mostraTempo = new JTextField();
-	
+
 	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
 	private JTextField mostraTempo2 = new JTextField();
-	
+
 	private JButton jButton = new JButton("Start");
 	private JButton jButton2 = new JButton("Stop");
 	
+	private Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() { //Fica sempre rodando
+			while(true) {
+				mostraTempo.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").
+						format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+		}
+	};
+	
+	private Thread thread1Time;
 
-	public TelaTimeThread() { /*Executa o que tiver dentro no momento da abertura ou execução*/
-		
-		setTitle("Minha tela de time com thread"); //titulo da janela
-		setSize(new Dimension(240,240)); //tamanho da janela
-		setLocationRelativeTo(null); //posicionamento da janela
+	public TelaTimeThread() { /* Executa o que tiver dentro no momento da abertura ou execução */
+
+		setTitle("Minha tela de time com thread"); // titulo da janela
+		setSize(new Dimension(240, 240)); // tamanho da janela
+		setLocationRelativeTo(null); // posicionamento da janela
 		setResizable(false); // travar tamanho da janela
-		/*Primeira parte concluída*/
-		
-		
-		
-		GridBagConstraints gridBagConstraints = new GridBagConstraints(); /* Controlador de posicionamento de componentes*/
+		/* Primeira parte concluída */
+
+		GridBagConstraints gridBagConstraints = new GridBagConstraints(); /*Controlador de posicionamento de componentes*/
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 0;
 		gridBagConstraints.gridwidth = 2;
 		gridBagConstraints.insets = new Insets(5, 10, 5, 5);
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
-		
-		descricaoHora.setPreferredSize(new Dimension(200,25));
+
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
 		jPanel.add(descricaoHora, gridBagConstraints);
-		
-		mostraTempo.setPreferredSize(new Dimension(200,25));
-		gridBagConstraints.gridy ++;
+
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
 		mostraTempo.setEditable(false);
 		jPanel.add(mostraTempo, gridBagConstraints);
-		
-		descricaoHora2.setPreferredSize(new Dimension(200,25));
-		gridBagConstraints.gridy ++;
+
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
 		jPanel.add(descricaoHora2, gridBagConstraints);
-		
-		mostraTempo2.setPreferredSize(new Dimension(200,25));
-		gridBagConstraints.gridy ++;
+
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
 		mostraTempo2.setEditable(false);
 		jPanel.add(mostraTempo2, gridBagConstraints);
-		
-		
-		//*************************************************************************
-		
-		gridBagConstraints.gridwidth = 1; /*Voltando o espaço do Grid para 1*/
-		
+
+		// *************************************************************************
+
+		gridBagConstraints.gridwidth = 1; /* Voltando o espaço do Grid para 1 */
+
 		jButton.setPreferredSize(new Dimension(92, 25));
-		gridBagConstraints.gridy ++;
+		gridBagConstraints.gridy++;
 		jPanel.add(jButton, gridBagConstraints);
-		
+
 		jButton2.setPreferredSize(new Dimension(92, 25));
-		gridBagConstraints.gridx ++;
+		gridBagConstraints.gridx++;
 		jPanel.add(jButton2, gridBagConstraints);
 		
+		jButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) { /*Executa o clique no botão*/
+				
+				thread1Time = new Thread(thread1);
+				thread1Time.start();
+				
+			}
+		});
 		
+		jButton2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				thread1Time.stop();
+				
+			}
+		});
+
 		add(jPanel, BorderLayout.WEST);
-		
-		/*Sempre será o último comando*/
-		setVisible(true); /* Torna a tela visível para o usuário*/
-		
+
+		/* Sempre será o último comando */
+		setVisible(true); /* Torna a tela visível para o usuário */
+
 	}
 
 }
