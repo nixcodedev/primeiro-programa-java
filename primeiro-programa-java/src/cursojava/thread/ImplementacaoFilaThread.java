978ad731ef1dc9d5ec6cc0446ed2ec923c67a3e6
nixcodedev <nixon.codedev@gmail.com>
2023-08-30 16:19:36 -0300

    Implementação do processo de Fila e Pilha com Thread - 3

--- primeiro-programa-java/src/cursojava/thread/ImplementacaoFilaThread.java
+++ primeiro-programa-java/src/cursojava/thread/ImplementacaoFilaThread.java
@@ -4,24 +4,24 @@ import java.util.Iterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ImplementacaoFilaThread extends Thread {
-	
-	private static ConcurrentLinkedQueue<ObjetoFilaThread> pilha_filha =
-							new ConcurrentLinkedQueue<ObjetoFilaThread>();
-	
+
+	private static ConcurrentLinkedQueue<ObjetoFilaThread> pilha_filha = new ConcurrentLinkedQueue<ObjetoFilaThread>();
+
 	public static void add(ObjetoFilaThread objetoFilaThread) {
 		pilha_filha.add(objetoFilaThread);
 	}
-	
-	
+
 	@Override
 	public void run() {
 		
 		System.out.println("Fila rodando...");
 		
-		Iterator iteracao = pilha_filha.iterator();
 		
-		synchronized (iteracao) { /*Bloqueia o acesso a esta lista por outros processos*/
+		while(true) {
+		
+		synchronized (pilha_filha) { /*Bloqueia o acesso a esta lista por outros processos*/
 			
+			Iterator iteracao = pilha_filha.iterator();
 			while (iteracao.hasNext()) { /*Enquanto conter dados na lista irá processar*/
 				
 				ObjetoFilaThread processar = (ObjetoFilaThread) iteracao.next(); // Pega o objeto atual
@@ -39,21 +39,25 @@ public class ImplementacaoFilaThread extends Thread {
 				iteracao.remove();
 				
 				try {
-					Thread.sleep(100); //Dar um tempo para a descarga de memória.
+					Thread.sleep(1000); //Dar um tempo para a descarga de memória.
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 			
 		}
-		
-		
 		try {
 			Thread.sleep(1000); //Processou toda a lista, dá um tempo para a descarga de memória
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
+		
+		
+		
+	 }
 				
-	}
 	
+
+}
 }
