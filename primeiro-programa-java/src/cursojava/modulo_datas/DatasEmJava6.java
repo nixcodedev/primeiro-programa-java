7a490df99a2e84175781f170bd1b991a0a79de9f
nixcodedev <nixon.codedev@gmail.com>
2023-09-21 12:18:16 -0300

    Formatando data e hora com nova API

--- primeiro-programa-java/src/cursojava/modulo_datas/DatasEmJava6.java
+++ primeiro-programa-java/src/cursojava/modulo_datas/DatasEmJava6.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class DatasEmJava6 {
 
@@ -13,19 +14,20 @@ public class DatasEmJava6 {
 
 		LocalDate dataAtual = LocalDate.now();
 
-		System.out.println("Data Atual " + dataAtual);
+		System.out.println("Data Atual " + dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 
 		
 		LocalTime horaAtual = LocalTime.now();
 
-		System.out.println("Hora atual: " + horaAtual);
+		System.out.println("Hora atual: " + horaAtual.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
 
 		
 		LocalDateTime dataAtualHoraAtual = LocalDateTime.now();
 
-		System.out.println("Data e Hora Atual: " + dataAtualHoraAtual);
+		System.out.println("Data e Hora Atual: " + dataAtualHoraAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
 		
 		
 	}
 
 }
+ 
