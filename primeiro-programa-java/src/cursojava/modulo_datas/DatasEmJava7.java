3e34ed32c3d786eb33837c7a68c918f8a333865f
nixcodedev <nixon.codedev@gmail.com>
2023-09-21 16:35:27 -0300

    Tempo de processo com Instant

--- primeiro-programa-java/src/cursojava/modulo_datas/DatasEmJava7.java
+++ primeiro-programa-java/src/cursojava/modulo_datas/DatasEmJava7.java
@@ -1,29 +1,32 @@
 package cursojava.modulo_datas;
 
 import java.text.ParseException;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DatasEmJava7 {
 
-	public static void main(String[] args) throws ParseException{
+	public static void main(String[] args) throws ParseException, InterruptedException {
+
+		Instant inicio = Instant.now();
+
+		Thread.sleep(2000); /*POde ser um processo com um tempo qualquer que não conhecemos, por isso a pausa de 2s*/
+
+		Instant ifinal = Instant.now();
+
+		Duration duracao = Duration.between(inicio, ifinal);
+
+		System.out.println("Duração em nanossegundos: " + duracao.toNanos());
 		
-		LocalDate localDate = LocalDate.now();
-		System.out.println("Data atual: " + localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		System.out.println("Duração em minutos: " + duracao.toMinutes());
 		
-		System.out.println("Dia da Semana: " + localDate.getDayOfWeek().name());
+		System.out.println("Duração em horas: " + duracao.toHours());
 		
+		System.out.println("Duração em milissegundos: " + duracao.toMillis());
 		
-		System.out.println("Dia do Mês: " + localDate.getDayOfMonth());
-		
-		
-		System.out.println("Dia do Ano: " + localDate.getDayOfYear());
-		
-		
-		System.out.println("Mês: " + localDate.getMonthValue());
-		
-		System.out.println("Ano: " + localDate.getYear());
-		
+		System.out.println("Duração em dias: " + duracao.toDays());
 
 	}
 
