797b832161dd2415d99a345c0852e495429df2a6
nixcodedev <nixon.codedev@gmail.com>
2023-09-22 16:38:43 -0300

    Data, anos, dias, meses e parcelas com LocalDate - Manipulando datas de boletos

--- primeiro-programa-java/src/cursojava/modulo_datas/DatasEmJava9.java
+++ primeiro-programa-java/src/cursojava/modulo_datas/DatasEmJava9.java
@@ -2,6 +2,7 @@ package cursojava.modulo_datas;
 
 import java.text.ParseException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DatasEmJava9 {
 
@@ -9,20 +10,15 @@ public class DatasEmJava9 {
 		
 		LocalDate dataBase = LocalDate.parse("2019-10-05");
 		
-		System.out.println("Mais 5 dias: " + (dataBase = dataBase.plusDays(5)));
+		for (int mes = 1; mes <=12; mes ++) {
+			dataBase = dataBase.plusMonths(1);
+			
+			System.out.println("Data de vencimento do boleto: " +
+			dataBase.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + " do mês: " + mes);
+			
+			
+		}
 		
-		System.out.println("Mais 5 semanas: : " + (dataBase = dataBase.plusWeeks(5)));
-		
-		System.out.println("Mais 5 anos:: " + (dataBase = dataBase.plusYears(5)));
-		
-		System.out.println("Mais 2 meses: " + (dataBase = dataBase.plusMonths(2)));
-		
-		
-		System.out.println("Menos 1 ano: " + (dataBase = dataBase.minusYears(1)));
-		
-		System.out.println("Menos 1 mês: " + (dataBase = dataBase.minusMonths(1)));
-		
-		System.out.println("Menos 20 dias: " + (dataBase = dataBase.minusDays(20)));
 
 	}
 
